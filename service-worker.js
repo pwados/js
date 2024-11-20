ce847a82360fda75d65fafea3bfd2539b7524f9f
pwados <behluliklit@gmail.com>
2024-11-20 08:38:39 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'dosbox-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './js-dos.js', // If using locally
+  './game.zip',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
